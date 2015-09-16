c334ed7de74755621381515f196333f56d491131
Jeff Heinrichs <jeff.heinrichs@icfi.com>
2015-09-15 21:35:10 -0500

    initial commit

---
+++ public/app/getWithGitControllers.js
@@ -0,0 +1,29 @@
+/*globals angular */
+
+(function () {
+    'use strict';
+
+    var origin = function ($scope, commonFunctions) {
+            $scope.test = 'test';
+
+            $scope.attempt = function () {
+                return 'attempt';
+            };
+
+            $scope.answer = commonFunctions.answer;
+        };
+
+    angular.module('getWithGit')
+           .factory('commonFunctions', function () {
+                return {
+                    'answer' : function (value1, value2) {
+                        console.log(value1, value2);
+                        var v1 = typeof value1 === 'number' ? value1 : 0,
+                            v2 = typeof value2 === 'number' ? value2 : 0;
+
+                        return v1 + v2;
+                    }
+                };
+           })
+           .controller('origin', ['$scope', 'commonFunctions', origin]);
+}());
